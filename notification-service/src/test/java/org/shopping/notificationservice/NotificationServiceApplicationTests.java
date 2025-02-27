2aac2b1c19461c899fd152f1412c8d41c6a1a8fb
anvesh-lp <knanvesh.01@gmail.com>
2025-02-27 16:47:50 -0600

    Implementing notification service and seniding emails

---
+++ notification-service/src/test/java/org/shopping/notificationservice/NotificationServiceApplicationTests.java
@@ -0,0 +1,14 @@
+package org.shopping.notificationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+
+class NotificationServiceApplicationTests extends AbstractIt {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
